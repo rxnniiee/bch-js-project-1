fba771655dc443a37046520e440a6bce00d57609
rxnniiee <23708969+rxnniiee@users.noreply.github.com>
2020-08-25 11:39:55 +0300

    done extra tasks

--- js/scripts.js
+++ js/scripts.js
@@ -36,4 +36,41 @@ function startScript() {
   `
 
   div_title_wrapper.appendChild(h1_title)
+
+  // Extra
+  const div_content = createElem('div')
+  div_content.setAttribute('id', 'content')
+
+  const ul_1 = createElem('ul')
+
+  const ul_content = [
+    { id: 'home',    href: 'default.asp',  content: 'Home'    },
+    { id: 'news',    href: 'news.asp',     content: 'News'    },
+    { id: 'contact', href: 'contact.asp',  content: 'Contact' },
+    { id: 'about',   href: 'about.asp',    content: 'About'   },
+  ]
+
+  ul_content.forEach(entry => {
+    const li  = createElem('li')
+    const a   = createElem('a')
+
+    a.setAttribute('id', entry.id)
+    a.setAttribute('href', entry.href)
+    a.classList.add('btn')
+    a.innerText = entry.content
+
+    li.appendChild(a)
+    ul_1.appendChild(li)
+  })
+
+  body.appendChild(ul_1)
+  body.appendChild(div_content)
+
+  select('#about').addEventListener('click', loadAbout)
+}
+
+function loadAbout(e) {
+  e.preventDefault() // not told to do, but we've got to see the change, right?
+  select('div#content').innerText = 'Welcome!'
+  select('div#title-wrapper').style.display = 'none'
 }
