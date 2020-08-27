8b55382637260c98bf51f5aeeab2fd17c673f5a2
rxnniiee <23708969+rxnniiee@users.noreply.github.com>
2020-08-27 10:45:23 +0300

    modularization

--- js/scripts.js
+++ js/scripts.js
@@ -8,33 +8,14 @@ const createElem  = e => document.createElement(e)
 function startScript() {
   console.log('Hello world')
 
-  document.body.style.backgroundImage = 'url("img/bg.jpg")'
+  setBackgroundImage('img/bg.jpg')
 
-  const div_title_wrapper = createElem('div')
+  const div_title_wrapper = createTitleWrapper()
   const body              = select('body')
 
-  div_title_wrapper.setAttribute('id', 'title-wrapper')
   body.appendChild(div_title_wrapper)
 
-  div_title_wrapper.style = `
-    top: 50%;
-    width: 100%;
-    text-align: center;
-    font-family: impact;
-    position: absolute;
-    text-transform: uppercase;
-  `
-
-  const h1_title = createElem('h1')
-  h1_title.setAttribute('id', 'title')
-  h1_title.innerText = 'Work under progress...'
-  h1_title.style = `
-    width: fit-content;
-    margin: auto;
-    background: black;
-    color: white;
-  `
-
+  const h1_title = createTitleElement('Work Under Progress...')
   div_title_wrapper.appendChild(h1_title)
 
   // Extra
@@ -50,20 +31,9 @@ function startScript() {
     { id: 'about',   href: 'about.asp',    content: 'About'   },
   ]
 
-  ul_content.forEach(entry => {
-    const li  = createElem('li')
-    const a   = createElem('a')
+  const ul = createUnorderedList(ul_content)
 
-    a.setAttribute('id', entry.id)
-    a.setAttribute('href', entry.href)
-    a.classList.add('btn')
-    a.innerText = entry.content
-
-    li.appendChild(a)
-    ul_1.appendChild(li)
-  })
-
-  body.appendChild(ul_1)
+  body.appendChild(ul)
   body.appendChild(div_content)
 
   select('#about').addEventListener('click', loadAbout)
@@ -73,4 +43,84 @@ function loadAbout(e) {
   e.preventDefault() // not told to do, but we've got to see the change, right?
   select('div#content').innerText = 'Welcome!'
   select('div#title-wrapper').style.display = 'none'
+}
+
+// modularize
+/**
+ * 
+ * @param {String} url either relative or absolute path to the image
+ * @returns {Void}
+ */
+function setBackgroundImage(url) {
+  if (!url) throw new Error('You must pass an url of the background image')
+  document.body.style.backgroundImage = `url(${url})`
+}
+
+/**
+ * @returns {HTMLDivElement} title wrapper
+ */
+function createTitleWrapper() {
+  const div_title_wrapper = createElem('div')
+  div_title_wrapper.setAttribute('id', 'title-wrapper')
+  div_title_wrapper.style = `
+    top: 50%;
+    width: 100%;
+    text-align: center;
+    font-family: impact;
+    position: absolute;
+    text-transform: uppercase;
+  `
+  return div_title_wrapper
+}
+
+/**
+ * 
+ * @param {String} text text to be displayed in the title
+ * @returns {HTMLHeadingElement} H1 with supplied text
+ */
+function createTitleElement(text) {
+  const h1_title = createElem('h1')
+  h1_title.setAttribute('id', 'title')
+  h1_title.innerText = text
+  h1_title.style = `
+    width: fit-content;
+    margin: auto;
+    background: black;
+    color: white;
+  `
+  return h1_title
+}
+
+/**
+ * 
+ * @param {Array.<{id: String, href: String, content: String}>} listContent list of entries
+ * @returns {HTMLUListElement}
+ */
+function createUnorderedList(listContent) {
+  const ul = createElem('ul')
+  listContent.forEach(item => {
+    const li  = createListItem(item.id, item.href, item.content)
+    ul.appendChild(li)
+  })
+  return ul
+}
+
+/**
+ * 
+ * @param {String} id set the ID of the element
+ * @param {String} href set the url of the anchor element
+ * @param {String} text text to be displayed as the link
+ * @returns {Void}
+ */
+function createListItem(id, href, content) {
+  const li  = createElem('li')
+  const a   = createElem('a')
+
+  a.setAttribute('id', id)
+  a.setAttribute('href', href)
+  a.classList.add('btn')
+  a.innerText = content
+
+  li.appendChild(a)
+  return li
 }
